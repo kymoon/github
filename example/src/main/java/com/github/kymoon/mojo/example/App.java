0d2e59e5dbe64eec16fb9178978a7f16061437ef
kymoon <kymoon@github>
2012-09-23 13:18:53 -0700

    Updated dependencies to Java 1.6.

--- example/src/main/java/com/github/kymoon/mojo/example/App.java
+++ example/src/main/java/com/github/kymoon/mojo/example/App.java
@@ -1,13 +1,7 @@
 package com.github.kymoon.mojo.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println(args[0]);
     }
 }
